8859406f20c343b40565867c772dc6813ecddc23
zhangzhijun.zzz <zhangzhijun.zzz@bytedance.com>
2024-01-17 12:10:55 +0800

    update

--- lua/hardline/themes/oxocarbon.lua
+++ lua/hardline/themes/oxocarbon.lua
@@ -5,7 +5,8 @@ local colors = {
 	base03 = { gui = "#525252", cterm = "59" },
 	base04 = { gui = "#dde1e6", cterm = "252" },
 	base05 = { gui = "#f2f4f8", cterm = "255" },
-	base06 = { gui = "#d4d4d4", cterm = "15" }, -- buffer inactive时status line字体颜色
+	--base06 = { gui = "#d4d4d4", cterm = "15" }, -- buffer inactive时status line字体颜色
+	base06 = { gui = "#767676", cterm = "15" }, -- buffer inactive时status line字体颜色
 	base07 = { gui = "#08bdba", cterm = "14" },
 	base08 = { gui = "#3ddbd9", cterm = "81" },
 	base09 = { gui = "#0078d4", cterm = "75" },
